d9efedf63d8503a066afb6b9b692cca419bc4491
ANDRE DO AMARAL <83513592+Andre-do-Amaral@users.noreply.github.com>
2021-12-03 10:37:53 -0300

    Update app.py

--- app.py
+++ app.py
@@ -109,6 +109,7 @@ if pagina == "Início":
 
     st.markdown("Web App para Análise Descritiva das informações nutricionais dos alimentos. Selecione o que deseja visualizar no menu ao lado.")
     ingredientes = max_todos_alimentos(nomes = nomes)
+    ingredientes
 
 elif pagina == "Análise Descritiva Produtos":
     data = datetime.datetime.now()
